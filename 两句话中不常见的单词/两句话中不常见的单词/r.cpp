767d16c0da612e4a091ab9515001315913a4cd1e
angle-99 <1971561926@qq.com>
2019-05-14 22:12:58 +0800

    两句话中不常见的单词

---
+++ 两句话中不常见的单词/两句话中不常见的单词/r.cpp
@@ -0,0 +1,42 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdlib.h>
+#include<iostream>
+#include<vector>
+#include<string>
+#include<unordered_map>
+using namespace std;
+
+class Solution
+{
+public:
+	vector<string> uncommonFromSentences(string A, string B)
+	{
+		string s = A + ' ' + B;
+		vector<string> v;
+		int start = 0;
+		int last = 0;
+		string words;
+		do
+		{
+			last = s.find(' ', start);
+			words = s.substr(start, last - start);
+			v.push_back(words);
+			start = last + 1;
+		} while (-1 != last);
+		unordered_map<string, int> m;
+		for (auto &e : v)
+			m[e]++;
+		vector<string> str;
+		for (auto &e : m)
+		{
+			if (e.second == 1)
+				str.push_back(e.first);
+		}
+		return str;
+	}
+};
+int main()
+{
+	system("pause");
+	return 0;
+}
