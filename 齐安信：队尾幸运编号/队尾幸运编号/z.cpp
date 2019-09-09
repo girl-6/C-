af376e3a846bfb1718d8e9409c43763d38f5437d
angle-99 <1971561926@qq.com>
2019-09-09 21:54:16 +0800

    待修改

--- 齐安信：队尾幸运编号/队尾幸运编号/z.cpp
+++ 齐安信：队尾幸运编号/队尾幸运编号/z.cpp
@@ -1,79 +1,37 @@
-//#include <iostream>
-//using namespace std;
-//
-//int Num(int n)
-//{
-//	int ret = n;
-//	int num = n / 5;
-//	n = n - n / 5 + n % 5;
-//	while (n % 5 != 0)
-//	{
-//		num += n / 5;
-//		ret -= num;
-//		if (n>5)
-//			n=ret+n%5;
-//		else
-//		{
-//			n = n % 5 + ret;
-//		}
-//	}
-//	num += n / 5;
-//	return num;
-//}
-//int main()
-//{
-//    int n;
-//	while (cin >> n)
-//	{
-//		if (n % 5 == 0)
-//			cout << n / 5 << endl;
-//		else
-//			cout << Num(n) << endl;
-//	}
-//	system("pause");
-//	return 0;
-//}
-
 #include <iostream>
-#include <vector>
-#include <string>
-
 using namespace std;
 
-
-int max_length_substring(string s) {
-	if (s.size() == 0)
-		return 0;
-	int len = 1;
-	string ret;
-	ret.push_back(s[0]);
-	for (int i = 1; i<s.size(); i++)
+int Num(int n)
+{
+	int ret = n;
+	int num = n / 5;
+	n = n - n / 5 + n % 5;
+	while (n % 5 != 0)
 	{
-		int pos = ret.find(s[i]);
-		if (pos==-1)
-			ret.push_back(s[i]);
+		num += n / 5;
+		ret -= num;
+		if (n>5)
+			n=ret+n%5;
 		else
 		{
-			len = ret.size()>len ? ret.size():len;
-			ret.erase( ret.begin() + pos);
-			ret.push_back(s[i]);
-			//cout << ret << endl;
+			n = n % 5 + ret;
 		}
 	}
-	len = ret.size()>len ? ret.size() : len;
-	return len;
+	num += n / 5;
+	return num;
 }
-
-
-int main() {
-	int res;
-
-	string _s;
-	getline(cin, _s);
-
-	res = max_length_substring(_s);
-	cout << res << endl;
+int main()
+{
+    int n;
+	while (cin >> n)
+	{
+		if (n % 5 == 0)
+			cout << n / 5 << endl;
+		else
+			cout << Num(n) << endl;
+	}
 	system("pause");
 	return 0;
-
 }
+
+
